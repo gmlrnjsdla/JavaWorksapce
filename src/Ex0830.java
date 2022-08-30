afc87ed2773fd9839035d32867dd201e467968f2
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 09:58:09 +0900

    Day2

--- src/Ex0830.java
+++ src/Ex0830.java
@@ -1,28 +1,24 @@
+//사각형의 넓이 구하기
 import java.util.Scanner;
-
 public class Ex0830 {
-	
-	
 	public static double area(double i, double j) {
 		return i * j;
-	      }
+	}
 	      
 	public static void main(String[] args) {
-		double result;
 		System.out.println("가로, 세로의 길이를 입력하세요 ");
 		
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.print("가로 : ");
-	      double width = scanner.nextDouble();
-	      
+	      int width = scanner.nextInt();
 	    System.out.print("세로 : ");
-	      double height = scanner.nextDouble();
+	      int height = scanner.nextInt();
 	      
 	    System.out.println("가로는 " + width + "cm,세로는 " 
                   + height + "cm 입니다.");
 
-	    result = area(width,height);
+	    double result = area(width,height);
 	    System.out.println("넓이는 " + result + "입니다.");
 
 	    scanner.close();
