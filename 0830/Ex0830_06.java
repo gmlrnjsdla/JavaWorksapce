65049906c438f2eddb6218b440e2918207c4bb91
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 14:11:05 +0900

    0830

--- 0830/Ex0830_06.java
+++ 0830/Ex0830_06.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 public class Ex0830_06 {
 
 	public static void main(String[] args) {
@@ -21,8 +21,21 @@ public class Ex0830_06 {
 //		System.out.println(10 < 3 || 10 > 3);
 //		System.out.println(10 < 3 || 10 < 3);
 		
-		System.out.println(!(10 > 3));
-		System.out.println(!(10 < 3));
+//		System.out.println(!(10 > 3));
+//		System.out.println(!(10 < 3));
+		
+		//a ? b : c ==> if~else
+		Scanner sc = new Scanner(System.in);
+		System.out.println("세 숫자를 입력하세요. : ");
+		int max;
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		max = a > b ? a : b;
+		max = max > c ? max : c;
+		System.out.println("최대값은 " + max + "입니다.");
+		
+		sc.close();
 	
 	}
 
