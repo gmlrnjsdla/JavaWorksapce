0e921958a1ae38dd2a0fe4dbf81882f2f0539ea0
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 10:37:45 +0900

    Day2

---
+++ src/Ex0830_01.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Ex0830_01 {
+
+	public static void main(String[] args) {
+		int su1, su2;
+	      double result; // 변수 선언
+	      Scanner scanner = new Scanner(System.in);
+	      su1 = scanner.nextInt(); // 변수 초기화
+	      su2 = scanner.nextInt(); // 변수 초기화
+	      result = su1 + su2;
+	      System.out.println(su1 + "+" + su2 + "=" + result); //10+20=30출력
+	      result = su1 - su2;
+	      System.out.println(su1 + "-" + su2 + "=" + result);
+	      result = su1 * su2;
+	      System.out.println(su1 + "*" + su2 + "=" + result); //10+20=30출력
+	      result = (double)su1 / su2; // 정수/정수=정수
+	      System.out.println(su1 + "/" + su2 + "=" + result);
+	      
+	      scanner.close();
+	}
+
+}
