6c3b81bfedaf74e55e808e69067d18f5e20a6269
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 11:46:49 +0900

    Day2

---
+++ src/Ex0830_04.java
@@ -0,0 +1,13 @@
+//연산자 연습.
+public class Ex0830_04 {
+
+	public static void main(String[] args) {
+		int num = 10;
+		//++num;
+		System.out.println(num++);
+		System.out.println(num);
+		System.out.println(num--);
+		System.out.println(num);
+	}
+
+}
