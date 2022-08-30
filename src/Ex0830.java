1b44be07cf6f8573810891b7bde4bd38549bcdc9
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 09:43:00 +0900

    Day2

--- src/Ex0830.java
+++ src/Ex0830.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
 
 public class Ex0830 {
-
+	
+	
+	public static double area(double i, double j) {
+		return i * j;
+	      }
+	      
 	public static void main(String[] args) {
+		double result;
+		System.out.println("가로, 세로의 길이를 입력하세요 ");
 		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("가로 : ");
+	      double width = scanner.nextDouble();
+	      
+	    System.out.print("세로 : ");
+	      double height = scanner.nextDouble();
+	      
+	    System.out.println("가로는 " + width + "cm,세로는 " 
+                  + height + "cm 입니다.");
+
+	    result = area(width,height);
+	    System.out.println("넓이는 " + result + "입니다.");
+
+	    scanner.close();
 	}
 
 }
