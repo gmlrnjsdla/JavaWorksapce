bbdff5526e129845afa5c668e46a4567c055ad16
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 11:26:56 +0900

    Day2

--- src/Ex0830_03.java
+++ src/Ex0830_03.java
@@ -4,8 +4,9 @@ public class Ex0830_03 {
 	public static void main(String[] args) {
 		int r = 5;
 		double area;
+		final double PI = 3.14;
 		
-		area = r*r*3.14;
+		area = r*r*PI;
 		System.out.println("원의 넓이는 : " + area );
 
 	}
