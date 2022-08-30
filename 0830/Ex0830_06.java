3c1f0092945658daa341c45ece1d9f4aed5b52a1
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 12:38:09 +0900

    0830

---
+++ 0830/Ex0830_06.java
@@ -0,0 +1,26 @@
+
+public class Ex0830_06 {
+
+	public static void main(String[] args) {
+		//비교연산자(관계연산자) >, <, >=, <=, ==, != true/false
+//		System.out.println(10 > 3);
+//		System.out.println(10 < 3);
+//		System.out.println(10 >= 3);
+//		System.out.println(10 <= 3);
+//		System.out.println(10 == 3);
+//		System.out.println(10 != 3);
+		
+		//논리연산자 : &&(and), ||(or), !(not)
+//		System.out.println(10 > 3 && 10 > 3);
+//		System.out.println(10 > 3 && 10 < 3);
+//		System.out.println(10 < 3 && 10 > 3);
+//		System.out.println(10 < 3 && 10 < 3);
+		
+		System.out.println(10 > 3 || 10 > 3);
+		System.out.println(10 > 3 || 10 < 3);
+		System.out.println(10 < 3 || 10 > 3);
+		System.out.println(10 < 3 || 10 < 3);
+	
+	}
+
+}
