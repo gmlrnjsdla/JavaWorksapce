8133fc12d53187ff07c506f28b48f8730a940ce8
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-30 11:37:47 +0900

    Day2

--- src/Ex0830_03.java
+++ src/Ex0830_03.java
@@ -1,14 +1,21 @@
+import java.util.Scanner;
+
 //반지름이 5cm인 원의 넓이를 구하시오.
 public class Ex0830_03 {
 
 	public static void main(String[] args) {
-		int r = 5;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("반지름(cm)을 입력해주세요: ");
+		int r = sc.nextInt();
 		double area;
 		final double PI = 3.14;
 		
 		area = r*r*PI;
-		System.out.println("원의 넓이는 : " + area );
-
+		System.out.println("원의 넓이는 : " + area +"㎤");
+		area = 2*r*PI;
+		System.out.println("원의 둘레는 : " + area + "cm");
+		
+		sc.close();
 	}
 
 }
