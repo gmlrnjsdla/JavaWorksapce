3cdd6d7bc46d812cd84a2186cc892a168e49344a
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-31 15:29:44 +0900

    0831

--- 0831/Ex0831_02.java
+++ 0831/Ex0831_02.java
@@ -2,30 +2,79 @@ import java.util.Scanner;
 
 public class Ex0831_02 {
 	public static void main (String[] args) {
-		Scanner scanner = new Scanner(System.in);
+//		int channel;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("당신이 좋아하는 채널 번호는?");
+//		channel = sc.nextInt();
+//		
+//		switch(channel) {
+//		case 11:
+//			System.out.println("MBC");
+//			break;
+//		case 9:
+//		case 7:
+//			System.out.println("KBS");
+//			break;
+//		case 5:
+//			System.out.println("SBS");
+//		break;
+//		default:
+//			System.out.println("정보가 없습니다.");
+//		}
+//		
+//		sc.close();
+//===================================================================//
 		
-		char grade;
-		System.out.print("점수를 입력하세요(0~100): ");
-		int score = scanner.nextInt();
-		switch (score/10) {
-			case 10: // score = 100
-			case 9: // score는 90~99
-				grade = 'A'; 
-				break;
-			case 8: // score는 80~89
-				grade = 'B';
-				break;
-			case 7: // score는 70~79
-				grade = 'C';
-				break;
-			case 6: // score는 60~69
-				grade = 'D';
-				break;
-			default: // score는 59 이하
-				grade = 'F';
-		}
-		System.out.println("학점은 "+grade+"입니다");
+//		String word;
+//		Scanner sc = new Scanner(System.in);
+//		System.out.print("단어 입력 :");
+//		word = sc.next();
+//		
+//		switch(word) {
+//		case "candy":
+//			System.out.println("사탕");
+//			break;
+//		case "apple":
+//			System.out.println("사과");
+//			break;
+//		case "pencil":
+//			System.out.println("연필");
+//			break;
+//		case "face":
+//			System.out.println("얼굴");
+//		break;
+//		default:
+//			System.out.println("정보가 없습니다.");
+//		}
+//		
+//		sc.close();
 
-		scanner.close();
+//=================================================================//
+		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		char grade;
+//		System.out.print("점수를 입력하세요(0~100): ");
+//		int score = scanner.nextInt();
+//		switch (score/10) {
+//			case 10: // score = 100
+//			case 9: // score는 90~99
+//				grade = 'A'; 
+//				break;
+//			case 8: // score는 80~89
+//				grade = 'B';
+//				break;
+//			case 7: // score는 70~79
+//				grade = 'C';
+//				break;
+//			case 6: // score는 60~69
+//				grade = 'D';
+//				break;
+//			default: // score는 59 이하
+//				grade = 'F';
+//		}
+//		System.out.println("학점은 "+grade+"입니다");
+//
+//		scanner.close();
 	}
 }
