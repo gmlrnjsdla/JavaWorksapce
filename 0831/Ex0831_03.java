e25f3cb9fe05c3b5d6ef283bbfa35d13025bb7a6
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-08-31 17:07:15 +0900

    0831

--- 0831/Ex0831_03.java
+++ 0831/Ex0831_03.java
@@ -46,10 +46,11 @@ public class Ex0831_03 {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("숫자1: " );
 		int a = sc.nextInt();
-		System.out.print("숫자2: " );
-		int b = sc.nextInt();
 		System.out.print("연산자( *, +, -, /, %): " );
 		String op = sc.next();
+		System.out.print("숫자2: " );
+		int b = sc.nextInt();
+		
 		
 		switch(op) {
 		case "*":
