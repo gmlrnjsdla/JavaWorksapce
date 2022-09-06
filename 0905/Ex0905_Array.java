db962bded7143318a7387f6e75b11aacb015a400
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-06 12:47:04 +0900

    0905

--- 0905/Ex0905_Array.java
+++ 0905/Ex0905_Array.java
@@ -3,7 +3,19 @@ public class Ex0905_Array {
 
 	public static void main(String[] args) {
 		// 배열 : 동일한 자료형들의 집합
-
+//		int i;
+//		int arr[]= {10, 20, 30};
+//		for(i =0; i<arr.length; i++) {
+//			System.out.println(a[i]);
+//		}
+		
+//=======================================================//
+		
+		
+		
+		
+		
+		
+		
 	}
-
 }
