b7d91d942255192e14902f46215a0d514e66e653
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-19 15:22:35 +0900

    0919

--- 0919/Ex0919_07.java
+++ 0919/Ex0919_07.java
@@ -16,25 +16,28 @@ private static void Check() {
     int year = (int)(Math.random()*99);          
     int day;
     
-    Jumin[6] = (int)(Math.random()*4)+1; // 성별
-    
+  
+    // 7번째 자리수가 1,2일때 19** , 3,4일때 20**
+    Jumin[6] = (int)(Math.random()*4)+1; // 성별   
     if(Jumin[6]== 1 || Jumin[6] == 2)
     	year1 = year + 1900;
     else if(Jumin[6]== 3 || Jumin[6] == 4)
     	year1 = year + 2000;
     
-    int month = (int)(Math.random()*12)+1; //월
     
+    
+    int month = (int)(Math.random()*12)+1; //월
+    //윤년 계산 , 일
     if(month == 2) {
-    	if(year1%4==0 && year1%100!=0 || year1%400==0) // 윤년 2월
+    	if(year1%4==0 && year1%100!=0 || year1%400==0) //2월, 윤년일때
         	day = (int)(Math.random()*++lastDay[month-1])+1;
-        else
+        else //2월, 윤년 아닐때
         	day = (int)(Math.random()*lastDay[month-1])+1;
     }
-    else {
+    else //2월이 아닐때
     	 day = (int)(Math.random()*lastDay[month-1])+1;
-    }
-     
+    
+    
         
     Jumin[0] = year/10;
     Jumin[1] = year%10;   
@@ -42,6 +45,7 @@ private static void Check() {
     Jumin[3] = month%10;  
     Jumin[4] = day/10;
     Jumin[5] = day%10; 
+    
     for(i=7;i<12;i++)  // 성별 이후~
        Jumin[i] = (int)(Math.random()*10);
     
