3c79668bec08646ce3f6c60dcdcfff98653a4583
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-09-19 12:03:23 +0900

    0919

--- 0919/Ex0919_05.java
+++ 0919/Ex0919_05.java
@@ -2,9 +2,19 @@
 public class Ex0919_05 {
 
 	public static void main(String[] args) {
-		int arr[] = new int [10];
-		arr[10] = 10;
-		System.out.println(10/0);
+		int a = 10, b = 10;
+		int arr[] = new int[5];
+		try {
+			System.out.println(a/b);
+			arr[50] = 10;
+		}catch (ArithmeticException e) {
+			System.out.println("0으로 나누면 안됩니다.");
+		}catch(ArrayIndexOutOfBoundsException e1){
+			System.out.println("첨차 범위 오류");
+		}
+		finally{
+			System.out.println("파일을 닫습니다.");
+		}
 	}
 
 }
